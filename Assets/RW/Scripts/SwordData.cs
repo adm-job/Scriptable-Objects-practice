871ddd31f30a8ef74f969f82e6d716d9ec549dc1
Stanislav <adm.job@yandex.ru>
2026-09-15 16:49:09 +0500

    new script

---
+++ Assets/RW/Scripts/SwordData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New SwordData", menuName = "Sword Data", order = 51)]
+public class SwordData : ScriptableObject
+{
+    [SerializeField]
+    private string swordName;
+    [SerializeField]
+    private string description;
+    [SerializeField]
+    private Sprite icon;
+    [SerializeField]
+    private int goldCost;
+    [SerializeField]
+    private int attackDamage;
+}
