d97b8cd8e3e892eaeafdefc1cd1777cde52e49c6
Stanislav <adm.job@yandex.ru>
2026-09-16 11:58:33 +0500

    add new class

--- Assets/RW/Scripts/GameEvent.cs
+++ Assets/RW/Scripts/GameEvent.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameEvent : MonoBehaviour
+[CreateAssetMenu(fileName = "New Game Event", menuName = "Game Event", order = 52)] // 1
+public class GameEvent : ScriptableObject // 2
 {
-    // Start is called before the first frame update
-    void Start()
+    private List<GameEventListener> listeners = new List<GameEventListener>(); // 3
+
+    public void Raise() // 4
     {
-        
+        for (int i = listeners.Count - 1; i >= 0; i--) // 5
+        {
+            listeners[i].OnEventRaised(); // 6
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RegisterListener(GameEventListener listener) // 7
     {
-        
+        listeners.Add(listener);
+    }
+
+    public void UnregisterListener(GameEventListener listener) // 8
+    {
+        listeners.Remove(listener);
     }
 }
--- Assets/RW/Scripts/GameEventListener.cs
+++ Assets/RW/Scripts/GameEventListener.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameEventListener : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameEvent gameEvent; // 2
+    [SerializeField]
+    private UnityEvent response; // 3
+
+    private void OnEnable() // 4
     {
-        
+        gameEvent.RegisterListener(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable() // 5
     {
-        
+        gameEvent.UnregisterListener(this);
+    }
+
+    public void OnEventRaised() // 6
+    {
+        response.Invoke();
     }
 }
